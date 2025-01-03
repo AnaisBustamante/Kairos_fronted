e0297a3f21cf80a37a7762cfab03b02bdc76819d
Anais <anaisbustamantetorres@gmail.com>
2025-01-03 00:03:46 -0500

    fondo en el login

--- lib/DashboardResponsive/dashboard.dart
+++ lib/DashboardResponsive/dashboard.dart
@@ -1,26 +1,13 @@
 import 'package:flutter/material.dart';
-//import 'package:kairos_fronted/DashboardResponsive/utils.dart';
+import 'package:kairos_fronted/DashboardResponsive/utils.dart';
 
 class Dashboard extends StatelessWidget {
   const Dashboard({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/images/background_image.png'), // Ruta de la imagen
-            fit: BoxFit.cover, // Ajusta la imagen al tamaño del contenedor
-          ),
-        ),
-        child: const Center(
-          child: Text(
-            'Dashboard',
-            style: TextStyle(fontSize: 24, color: Colors.white),
-          ),
-        ),
-      ),
+    return const Scaffold(
+      backgroundColor: MyAppColor.backgroundColor,
     );
-  }
+    }
 }
+  
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:kairos_fronted/DashboardResponsive/dashboard.dart';
+import 'package:kairos_fronted/LoginResponsive/login.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Dashboard(),
+      home: Login(),
     );
   }
 }
